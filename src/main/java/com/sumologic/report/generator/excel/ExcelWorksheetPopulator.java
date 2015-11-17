9e24a536648ce5ad41f34044f9f9d021510cbbba
Frank Reno <frank.reno@me.com>
2015-11-17 10:19:06 -0700

    fix #20, if a field is a numeric type, then let's be sure we correctly set the excel format.

--- src/main/java/com/sumologic/report/generator/excel/ExcelWorksheetPopulator.java
+++ src/main/java/com/sumologic/report/generator/excel/ExcelWorksheetPopulator.java
@@ -8,6 +8,7 @@ import com.sumologic.report.config.ReportSheet;
 import com.sumologic.service.SumoDataService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.springframework.stereotype.Service;
@@ -62,12 +63,47 @@ public class ExcelWorksheetPopulator implements WorksheetPopulator {
             for (SearchJobField field : recordsResponse.getFields()) {
                 int colIndex = ExcelUtils.lookupCellIndexByColumnName(field.getName(), workbookSheet);
                 LOGGER.trace("creating cell " + colIndex + " with value " + record.getMap().get(field.getName()));
-                row.createCell(colIndex).setCellValue(record.getMap().get(field.getName()));
+                createCell(record, row, field, colIndex);
             }
             rowIndex++;
         }
     }
 
+    private void createCell(SearchJobRecord record, Row row, SearchJobField field, int colIndex) {
+        Cell cell = row.createCell(colIndex);
+        String cellValue = record.getMap().get(field.getName());
+        if (isNumericField(field)) {
+            cell.setCellType(Cell.CELL_TYPE_NUMERIC);
+            if (isDoubleField(field)) {
+                cell.setCellValue(Double.parseDouble(cellValue));
+            } else if (isLongField(field)) {
+                cell.setCellValue(Long.parseLong(cellValue));
+            } else if (isIntField(field)) {
+                cell.setCellValue(Integer.parseInt(cellValue));
+            } else {
+                LOGGER.error("unknown field type " + field.getFieldType());
+            }
+        } else {
+            cell.setCellType(Cell.CELL_TYPE_STRING);
+            cell.setCellValue(cellValue);
+        }
+    }
 
+
+    private boolean isNumericField(SearchJobField field) {
+        return isIntField(field) || isLongField(field) || isDoubleField(field);
+    }
+
+    private boolean isDoubleField(SearchJobField field) {
+        return field.getFieldType().equals("double");
+    }
+
+    private boolean isLongField(SearchJobField field) {
+        return field.getFieldType().equals("long");
+    }
+
+    private boolean isIntField(SearchJobField field) {
+        return field.getFieldType().equals("int");
+    }
 
 }
